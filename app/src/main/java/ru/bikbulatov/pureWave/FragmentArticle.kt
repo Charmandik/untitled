df1a5a650111d9866b4b20c94c2537a269b49efd
Charmandik <rbikbulatov@list.ru>
2020-08-23 23:20:23 +0500

    added viewmodel and created some fragment classes

---
+++ app/src/main/java/ru/bikbulatov/pureWave/FragmentArticle.kt
@@ -0,0 +1,24 @@
+package ru.bikbulatov.pureWave
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ru.bikbulatov.pureWave.databinding.FragmentArticleBinding
+
+class FragmentArticle : Fragment() {
+    private lateinit var binding: FragmentArticleBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentArticleBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/ru/bikbulatov/pureWave/MainViewModel.kt
@@ -0,0 +1,6 @@
+package ru.bikbulatov.pureWave
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+}
