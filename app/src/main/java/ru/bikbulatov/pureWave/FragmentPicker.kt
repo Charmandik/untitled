33b7021197e2199b11b03075bb5e7f578f9b8754
rustem <bikbulatov_r@ufanet.ru>
2020-08-26 09:22:33 +0500

    init

---
+++ app/src/main/java/ru/bikbulatov/pureWave/FragmentPicker.kt
@@ -0,0 +1,25 @@
+package ru.bikbulatov.pureWave
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ru.bikbulatov.pureWave.databinding.FragmentAuthorsBinding
+import ru.bikbulatov.pureWave.databinding.FragmentPickerBinding
+
+class FragmentPicker : Fragment() {
+    private lateinit var binding: FragmentPickerBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPickerBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        return binding.root
+    }
+}
--- app/src/main/java/ru/bikbulatov/pureWave/MainActivity.kt
+++ app/src/main/java/ru/bikbulatov/pureWave/MainActivity.kt
@@ -2,10 +2,53 @@ package ru.bikbulatov.pureWave
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.bikbulatov.pureWave.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        configureBottomNavigation()
+    }
+
+    private fun configureBottomNavigation() {
+        binding.bnvMain.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_media -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.flContainer.id, FragmentPicker())
+                        .commit()
+                }
+                R.id.navigation_music -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.flContainer.id, FragmentProgramsCategories())
+                        .commit()
+                }
+                R.id.navigation_main -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.flContainer.id, FragmentPlayer())
+                        .commit()
+                }
+                R.id.navigation_article -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.flContainer.id, FragmentArticle())
+                        .commit()
+                }
+                R.id.navigation_search -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.flContainer.id, FragmentSearch())
+                        .commit()
+                }
+            }
+            true
+        }
+
     }
 }
